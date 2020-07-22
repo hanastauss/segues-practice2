3be32f6875ede87e48111bdc7939579a595b8922
Hana Stauss <hanastauss@icloud.com>
2020-07-22 15:40:29 -0700

    Initial Commit

---
+++ segues-practice2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segues-practice2
+//
+//  Created by Hana Stauss on 7/22/20.
+//  Copyright © 2020 Hana Stauss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
